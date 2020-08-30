30a9f65c67361db765d380fc5702bf97ca490f15
pskmax <pskma@LAPTOP-810SC5FO>
2020-08-30 16:33:42 +0700

    add 008

---
+++ Weekly_Homework/008.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	char a[1000];
+	scanf("%s", &a);
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u')
+		{
+			printf("There is vowel");
+			return 0;
+		}
+	}
+	printf("There is not vowel");
+	return 0;
+}
