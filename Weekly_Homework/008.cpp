2b51885fe17ad7e18937189706b452b67e0103f3
pskmax <pskma@LAPTOP-810SC5FO>
2020-09-02 12:14:54 +0700

    add 1000 in scanf

--- Weekly_Homework/008.cpp
+++ Weekly_Homework/008.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	char a[1000];
-	scanf("%s", &a);
+	scanf("%s", &a, 1000);
 	for (int i = 0; a[i] != '\0'; i++)
 	{
 		if (a[i] == 'a' || a[i] == 'e' || a[i] == 'i' || a[i] == 'o' || a[i] == 'u')
